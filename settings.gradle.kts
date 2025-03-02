9c69bc44c8ea9473dc5fbc24ccda42afdef7c2e6
Kyle Lin <minecraft.kyle.train@gmail.com>
2025-03-02 19:06:24 +0800

    Update Simplified Chinese Translation, bump version 1.0.1

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -30,5 +30,4 @@ dependencyResolutionManagement {
 }
 
 // Due to an IntelliJ bug, this has to be done
-// rootProject.name = archives_base_name
-rootProject.name = rootProject.projectDir.getName()
+ rootProject.name = "Retro-Sophisticated-Backpacks"
